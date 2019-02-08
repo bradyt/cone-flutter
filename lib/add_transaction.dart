2eb37a5a9ec01f8902aecda6fc3d1a8c24a5d670
Brady Trainor <mail@bradyt.com>
2019-02-07 22:40:47 -0800

    Add changes

--- lib/add_transaction.dart
+++ lib/add_transaction.dart
@@ -7,6 +7,8 @@ import 'package:package_info/package_info.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 
+import 'package:cone/transaction.dart';
+
 enum Fields { date, description, account, amount, currency }
 
 List<String> accounts = [
@@ -34,27 +36,10 @@ List<String> currencies = [
   'NZD',
 ];
 
-String showTransaction(
-  String date,
-  String description,
-  String account1,
-  String amount1,
-  String currency1,
-  String account2,
-  String amount2,
-  String account3,
-  String amount3,
-  String account4,
-  String amount4,
-) {
-  return '''
-
-$date $description
-  $account1  $amount1 $currency1
-  $account2  $amount2 USD
-  $account3  $amount3 USD
-  $account4  $amount4 USD
-''';
+fieldFocusChange(
+    BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+  currentFocus.unfocus();
+  FocusScope.of(context).requestFocus(nextFocus);
 }
 
 // https://grokonez.com/flutter/flutter-read-write-file-example-path-provider-dartio-example
@@ -65,48 +50,24 @@ class AddTransaction extends StatefulWidget {
 class AddTransactionState extends State<AddTransaction> {
   var dateController = TextEditingController();
   var descriptionController = TextEditingController();
-  var account1Controller = TextEditingController();
-  var amount1Controller = TextEditingController();
-  var currency1Controller = TextEditingController();
-  var account2Controller = TextEditingController();
-  var amount2Controller = TextEditingController();
-  var account3Controller = TextEditingController();
-  var amount3Controller = TextEditingController();
-  var account4Controller = TextEditingController();
-  var amount4Controller = TextEditingController();
 
-  String date,
-      description,
-      account1,
-      amount1,
-      currency1,
-      account2,
-      amount2,
-      account3,
-      amount3,
-      account4,
-      amount4;
+  String date;
+  String description;
 
   final FocusNode dateFocus = FocusNode();
   final FocusNode descriptionFocus = FocusNode();
-  final FocusNode account1Focus = FocusNode();
-  final FocusNode amount1Focus = FocusNode();
-  final FocusNode currency1Focus = FocusNode();
-  final FocusNode account2Focus = FocusNode();
-  final FocusNode amount2Focus = FocusNode();
-  final FocusNode account3Focus = FocusNode();
-  final FocusNode amount3Focus = FocusNode();
-  final FocusNode account4Focus = FocusNode();
-  final FocusNode amount4Focus = FocusNode();
 
   var _formKey = GlobalKey<FormState>();
+
+  List<Posting> postings = [];
 
   void initState() {
     super.initState();
     dateController.text = DateFormat('yyyy-MM-dd').format(DateTime.now());
-    // account1Controller.text = 'expenses:';
-    currency1 = 'USD';
-    account2Controller.text = 'assets:checking';
+    postings = [
+      Posting(account: 'expenses:', amount: null, currency: 'USD'),
+      Posting(account: 'assets:checking', amount: null, currency: 'USD'),
+    ];
   }
 
   Widget build(BuildContext context) {
@@ -114,32 +75,12 @@ class AddTransactionState extends State<AddTransaction> {
       appBar: AppBar(
         title: Text('cone'),
         actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.save),
-            onPressed: () {
-              _formKey.currentState.save();
-              if (_formKey.currentState.validate()) {
-                String result = showTransaction(
-                  date,
-                  description,
-                  account1,
-                  amount1,
-                  currency1,
-                  account2,
-                  amount2,
-                  account3,
-                  amount3,
-                  account4,
-                  amount4,
-                );
-                final snackBar = SnackBar(
-                  content: Text(result),
-                );
-                TransactionStorage.writeTransaction(result);
-                Scaffold.of(context).showSnackBar(snackBar);
-              }
-            },
-          )
+          Builder(
+            builder: (context) => IconButton(
+                  icon: Icon(Icons.save),
+                  onPressed: () => submitTransaction(context),
+                ),
+          ),
         ],
       ),
       body: Form(
@@ -147,34 +88,134 @@ class AddTransactionState extends State<AddTransaction> {
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              dateFormField(context),
-              descriptionFormField(context),
-              postings(context),
-            ],
+              dateAndDescriptionWidget(context),
+            ]..addAll(
+                List<int>.generate(postings.length, (i) => i + 1).map((i) {
+                  final posting = postings[i];
+                  return Dismissible(
+                    key: posting.key,
+                    onDismissed: (direction) {
+                      setState(() {
+                        postings.removeAt(i);
+                      });
+                    },
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Account $i',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Amount $i',
+                              border: OutlineInputBorder(),
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Flexible(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Currency $i',
+                              border: OutlineInputBorder(),
+                            ),
+                            initialValue: 'USD',
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+              ),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => setState(() => addPosting()),
       ),
     );
   }
 
-  Widget postings(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        account1FormField(context),
-        Row(
-          children: <Widget>[
-            Expanded(
-              child: amount1FormField(context),
-            ),
-            currency1FormField(context),
-          ],
+  void addPosting() {
+    postings.add(Posting(account: null, amount: null, currency: null));
+    // numberOfPostings++;
+    // int n = postingSubforms.length;
+    // postingSubforms.add(Row(
+    //   children: [
+    //     Expanded(
+    //       child: TextFormField(
+    //         decoration: InputDecoration(
+    //           labelText: 'Account $n',
+    //           border: OutlineInputBorder(),
+    //         ),
+    //         initialValue:
+    //             (n == 0) ? 'expenses:' : ((n == 1) ? 'assets:checking' : null),
+    //       ),
+    //     ),
+    //     Expanded(
+    //       child: TextFormField(
+    //         decoration: InputDecoration(labelText: 'Amount $n'),
+    //         keyboardType: TextInputType.number,
+    //       ),
+    //     ),
+    //     Flexible(
+    //       child: TextFormField(
+    //         decoration: InputDecoration(labelText: 'Currency $n'),
+    //         initialValue: 'USD',
+    //       ),
+    //     ),
+    //   ],
+    // ));
+  }
+
+  void submitTransaction(BuildContext context) {
+    _formKey.currentState.save();
+    // Transaction txn = Transaction(
+    //   date,
+    //   description,
+    //   postingStuff
+    //       .map((it) => Posting(
+    //             'hello',
+    //             null,
+    //             null,
+    //           ))
+    //       .toList(),
+    // );
+    // print(txn);
+    // if (_formKey.currentState.validate()) {
+    //   String result = txn.toString();
+    //   final snackBar = SnackBar(
+    //     content: RichText(
+    //       text: TextSpan(
+    //         text: result,
+    //         style: TextStyle(
+    //           fontFamily: "RobotoMono",
+    //         ),
+    //       ),
+    //     ),
+    //   );
+    //   TransactionStorage.writeTransaction('\n\n' + result);
+    //   Scaffold.of(context).showSnackBar(snackBar);
+    // }
+  }
+
+  Row dateAndDescriptionWidget(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+          child: dateFormField(context),
         ),
-        // account2FormField(context),
-        // amount2FormField(context),
-        // account3FormField(context),
-        // amount3FormField(context),
-        account4FormField(context),
-        amount4FormField(),
+        Expanded(
+          child: descriptionFormField(context),
+        ),
       ],
     );
   }
@@ -191,6 +232,7 @@ class AddTransactionState extends State<AddTransaction> {
         date = value;
       },
       decoration: InputDecoration(
+        border: OutlineInputBorder(),
         labelText: 'Date',
         suffixIcon: IconButton(
           onPressed: () {
@@ -202,12 +244,6 @@ class AddTransactionState extends State<AddTransaction> {
         ),
       ),
     );
-  }
-
-  fieldFocusChange(
-      BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
-    currentFocus.unfocus();
-    FocusScope.of(context).requestFocus(nextFocus);
   }
 
   Future chooseDate(BuildContext context, String initialDateString) async {
@@ -251,156 +287,103 @@ class AddTransactionState extends State<AddTransaction> {
         description = value;
       },
       onFieldSubmitted: (term) {
-        fieldFocusChange(context, descriptionFocus, account1Focus);
+        descriptionFocus.unfocus();
       },
-      decoration: InputDecoration(labelText: 'Description'),
+      decoration: InputDecoration(
+        labelText: 'Description',
+        border: OutlineInputBorder(),
+      ),
     );
   }
+}
 
-  TextFormField account1FormField(BuildContext context) {
-    return TextFormField(
-      controller: account1Controller,
-      focusNode: account1Focus,
-      textInputAction: TextInputAction.next,
-      onSaved: (value) {
-        account1 = value;
-      },
-      onFieldSubmitted: (term) {
-        fieldFocusChange(context, account1Focus, amount1Focus);
-      },
-      decoration: InputDecoration(labelText: 'Account one'),
+// class AccountFormField extends TextFormField {
+//   AccountFormField(int index, )
+//           child: TextFormField(
+//             decoration: InputDecoration(labelText: 'Account $n'),
+//             initialValue:
+//                 (n == 0) ? 'expenses:' : ((n == 1) ? 'assets:checking' : null),
+//           ),
+
+// }
+
+class PostingFieldStuff {
+  TextEditingController accountController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
+  TextEditingController currencyController = TextEditingController();
+
+  final FocusNode accountFocus = FocusNode();
+  final FocusNode amountFocus = FocusNode();
+  final FocusNode currencyFocus = FocusNode();
+}
+
+class PostingForm extends StatefulWidget {
+  PostingFormState createState() => PostingFormState();
+}
+
+class PostingFormState extends State<PostingForm> {
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[],
     );
-  }
 
-  TextFormField amount1FormField(BuildContext context) {
-    return TextFormField(
-      controller: amount1Controller,
-      focusNode: amount1Focus,
-      textInputAction: TextInputAction.next,
-      validator: (value) {
-        if (value.isEmpty) {
-          return 'Please enter a first amount';
-        }
-      },
-      onSaved: (value) {
-        amount1 = value;
-      },
-      onFieldSubmitted: (term) {
-        fieldFocusChange(context, amount1Focus, account4Focus);
-      },
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(labelText: 'Amount one'),
-    );
-  }
+    //   TextFormField account1FormField(BuildContext context) {
+    //     return TextFormField(
+    //       controller: account1Controller,
+    //       focusNode: account1Focus,
+    //       textInputAction: TextInputAction.next,
+    //       onSaved: (value) {
+    //         account1 = value;
+    //       },
+    //       onFieldSubmitted: (term) {
+    //         fieldFocusChange(context, account1Focus, amount1Focus);
+    //       },
+    //       decoration: InputDecoration(labelText: 'Account one'),
+    //     );
+    //   }
 
-  DropdownButton currency1FormField(BuildContext context) {
-    return DropdownButton(
-        value: currency1,
-        items: currencies.map((String val) {
-          return DropdownMenuItem<String>(
-            value: val,
-            child: Text(val),
-          );
-        }).toList(),
-        // hint: Text('Enter currency one'),
-        iconSize: 40.0,
-        onChanged: (newVal) {
-          currency1 = newVal;
-          this.setState(() {});
-        });
-  }
+    //   ;
 
-  TextFormField account2FormField(BuildContext context) {
-    return TextFormField(
-      controller: account2Controller,
-      focusNode: account2Focus,
-      textInputAction: TextInputAction.next,
-      onSaved: (value) {
-        account2 = value;
-      },
-      onFieldSubmitted: (term) {
-        fieldFocusChange(context, account2Focus, amount2Focus);
-      },
-      decoration: InputDecoration(labelText: 'Account two'),
-    );
-  }
+    //   TextFormField amount1FormField(BuildContext context) {
+    //     return TextFormField(
+    //       controller: amount1Controller,
+    //       focusNode: amount1Focus,
+    //       textInputAction: TextInputAction.next,
+    //       validator: (value) {
+    //         if (value.isEmpty) {
+    //           return 'Please enter a first amount';
+    //         }
+    //       },
+    //       onSaved: (value) {
+    //         amount1 = value;
+    //       },
+    //       onFieldSubmitted: (term) {
+    //         fieldFocusChange(context, amount1Focus, account4Focus);
+    //       },
+    //       keyboardType: TextInputType.number,
+    //       decoration: InputDecoration(labelText: 'Amount one'),
+    //     );
+    //   }
 
-  TextFormField amount2FormField(BuildContext context) {
-    return TextFormField(
-      controller: amount2Controller,
-      focusNode: amount2Focus,
-      textInputAction: TextInputAction.next,
-      onSaved: (value) {
-        amount2 = value;
-      },
-      onFieldSubmitted: (term) {
-        fieldFocusChange(context, amount2Focus, account3Focus);
-      },
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(labelText: 'Enter amount two'),
-    );
-  }
+    //   DropdownButton currency1FormField(BuildContext context) {
+    //     return DropdownButton(
+    //         value: currency1,
+    //         items: currencies.map((String val) {
+    //           return DropdownMenuItem<String>(
+    //             value: val,
+    //             child: Text(val),
+    //           );
+    //         }).toList(),
+    //         // hint: Text('Enter currency one'),
+    //         iconSize: 40.0,
+    //         onChanged: (newVal) {
+    //           currency1 = newVal;
+    //           this.setState(() {});
+    //         });
+    //   }
 
-  TextFormField account3FormField(BuildContext context) {
-    return TextFormField(
-      controller: account3Controller,
-      focusNode: account3Focus,
-      textInputAction: TextInputAction.next,
-      onSaved: (value) {
-        account3 = value;
-      },
-      onFieldSubmitted: (term) {
-        fieldFocusChange(context, account3Focus, amount3Focus);
-      },
-      decoration: InputDecoration(labelText: 'Enter account three'),
-    );
-  }
-
-  TextFormField amount3FormField(BuildContext context) {
-    return TextFormField(
-      controller: amount3Controller,
-      focusNode: amount3Focus,
-      textInputAction: TextInputAction.next,
-      onSaved: (value) {
-        amount3 = value;
-      },
-      onFieldSubmitted: (term) {
-        fieldFocusChange(context, amount3Focus, account4Focus);
-      },
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(labelText: 'Enter amount three'),
-    );
-  }
-
-  TextFormField account4FormField(BuildContext context) {
-    return TextFormField(
-      controller: account4Controller,
-      focusNode: account4Focus,
-      textInputAction: TextInputAction.next,
-      onSaved: (value) {
-        account4 = value;
-      },
-      onFieldSubmitted: (term) {
-        fieldFocusChange(context, account4Focus, amount4Focus);
-      },
-      decoration: InputDecoration(labelText: 'Enter account four'),
-    );
-  }
-
-  TextFormField amount4FormField() {
-    return TextFormField(
-      controller: amount4Controller,
-      focusNode: amount4Focus,
-      textInputAction: TextInputAction.done,
-      onSaved: (value) {
-        amount4 = value;
-      },
-      onFieldSubmitted: (term) {
-        amount4Focus.unfocus();
-      },
-      keyboardType: TextInputType.number,
-      decoration: InputDecoration(labelText: 'Enter amount four'),
-    );
+    //   return null;
+    // }
   }
 }
 
@@ -423,21 +406,3 @@ class TransactionStorage {
     return file.writeAsString('$transaction', mode: FileMode.append);
   }
 }
-
-// class AccountSearch extends SearchDelegate<String> {
-//   List<Widget> buildActions(BuildContext context) {
-//     return;
-//   }
-
-//   Widget buildLeading(BuildContext context) {
-//     return;
-//   }
-
-//   Widget buildResults(BuildContext context) {
-//     return;
-//   }
-
-//   Widget buildSuggestions(BuildContext context) {
-//     return;
-//   }
-// }
---
+++ lib/transaction.dart
@@ -0,0 +1,37 @@
+class Transaction {
+  String date;
+  String description;
+  List<Posting> postings;
+
+  Transaction(this.date, this.description, this.postings);
+
+  String toString() {
+    String result = '$date $description';
+    if (postings.length > 0) {
+      result += '\n  ' +
+          postings
+              .map((ps) => ps.toString())
+              .where((it) => it != null)
+              .join('\n  ');
+    }
+    return result;
+  }
+}
+
+class Posting {
+  String account;
+  String amount;
+  String currency;
+
+  Posting({this.account, this.amount, this.currency});
+
+  String toString() {
+    if (account == null) {
+      return null;
+    } else if (amount == '') {
+      return '$account';
+    } else {
+      return '$account  $amount $currency';
+    }
+  }
+}
